81cd099671ee7f20c94ac5cd1339d01940f2434f
Alejandro Albino <webmaster@alexventure.com>
2025-01-07 18:44:44 -0800

    Print button now triggers window.print()

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,15 +75,17 @@ export default function Home() {
     setTotalColumns(newValue)
   }
 
+  function handleOnPrint()
+  {
+    window.print()
+  }
+
   return (
     <div className="flex flex-col justify-center items-center print:m-0 print:p-0">
       <main className="flex flex-col gap-4 items-center sm:items-start print:border-none print:m-0 print:p-0">
         <div className="grid grid-flow-col justify-start align-middle space-x-4 w-full border border-solid border-slate-700 text-white bg-slate-600 font-semibold drop-shadow-lg p-4 print:border-none print:drop-shadow-none print:hidden">
-          <div>Columns:
-            <input type="number" value={totalColumns} onChange={e => handleTotalColumnsUpdate(parseInt(e.target.value))} />
-          </div>
-          <PhotoButton />
-          <PrintButton />
+          
+          <PrintButton onClick={(ev) => handleOnPrint() } />
         </div>
         <div className="paper-legal border border-solid border-gray-300 border-1 drop-shadow-lg text-gray-600 px-[0.25in] py-[0.25in] flex flex-col print:m-0 print:drop-shadow-none print:border-none">
           <div className="editable-header border-dashed border-2 border-gray-300 hover:border-cyan-700 hover:bg-cyan-300 rounded-md row-span-1 p-2 text-lg print:border-none">
@@ -124,53 +126,7 @@ export default function Home() {
           </div>
         </div>
       </main>
-      <footer className="flex gap-8 flex-wrap items-center justify-center mt-4 print:hidden">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      
     </div>
   );
 }
